7b5d0700c47280d305d54d0c90a599540beab0cf
Randy Horman <rwhorman@gmail.com>
2020-12-16 21:28:58 -0500

    new serial device utility service

---
+++ services/serial/commands/devices_v1.py
@@ -0,0 +1,37 @@
+import os
+import serial.tools
+
+from command import Command, CommandInterface
+
+if os.name == 'posix':
+    from serial.tools.list_ports_posix import comports
+else:
+    raise ImportError(f'ERROR: platform ("{os.name}") not supported')
+
+class Devices(Command):
+
+    def __init__(self, owning_service, version):
+
+        self.owning_service = owning_service
+
+        self.interface = CommandInterface('devices', 'List serial devices', version, self,
+                                          {'--verbose': {'action':'store_true',
+                                                         'help':'additional detail on each device'},
+                                           '--symlinks': {'action':'store_true',
+                                                          'help':'include symlinks to real devices'},
+                                           '--entry': {'type':int,
+                                                       'help':'list the numbered entry'}
+                                            })
+
+    def execute(self, args, output_text):
+
+        output = []
+        if output_text:
+            devices= sorted(comports(include_links=args.symlinks))
+            output = [f'{port}' + (f'\n   ID={hwid}: {desc}' if args.verbose else '')
+                        for entry, (port, desc, hwid) in enumerate(devices, 1)
+                            if args.entry is None or args.entry == entry]
+        else:
+            pass
+
+        return output
